c48ba849d34646462a627092bbfafbd7ded1557c
Nijat-Guliyev <iamnijatguliyev@gmail.com>
2025-04-17 09:42:01 +0400

    Fixed SVG icon paths and updated deployment setup

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'fecf9393-5528-4542-bf8f-542b08be2e2c';
 export const MODAL_CLOSE_SEC = 2.5;
